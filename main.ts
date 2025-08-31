94052a442ff580b6ea589e39cc8903857a29d4ea
hugo2009fitbit-ctrl <hugo2009fitbit@gmail.com>
2025-08-31 11:50:23 +0200

    temperatuur en gezichtje

--- main.ts
+++ main.ts
@@ -9,6 +9,22 @@ input.onButtonPressed(Button.A, function () {
     RingbitCar.steering_angle(RingbitCar.Direction_turn.right, 90)
     basic.clearScreen()
 })
+input.onGesture(Gesture.Shake, function () {
+    temperatuur = input.temperature()
+    temperatuur += -3
+    basic.showString("" + (temperatuur))
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    basic.showLeds(`
+        . . . . .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+    basic.pause(2000)
+    basic.clearScreen()
+})
 input.onButtonPressed(Button.AB, function () {
     basic.showLeds(`
         . . # . .
@@ -31,4 +47,5 @@ input.onButtonPressed(Button.B, function () {
     RingbitCar.steering_angle(RingbitCar.Direction_turn.left, 90)
     basic.clearScreen()
 })
+let temperatuur = 0
 RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P0)
